0a4099555e1ce8a21a601b6a13b18187571ea7d6
velilha <32044809+velilha@users.noreply.github.com>
2026-03-10 02:47:25 -0300

    Update Otimizar.ps1

--- Otimizar.ps1
+++ Otimizar.ps1
@@ -1,15 +1,19 @@
-# Otimiza o sistema
 $u = $env:USERNAME
 $bp = "C:\Users\$u\Desktop\Ativar.bat"
+
+# Desativa serviços pesados e telemetria
 $s = @("wuauserv", "bits", "dosvc", "UsoSvc", "WSearch", "bthserv", "BthAvctpSvc", "Spooler", "DiagTrack", "FeedbackHub")
 foreach ($x in $s) {
     Stop-Service -Name $x -Force -ErrorAction SilentlyContinue
     Set-Service -Name $x -StartupType Disabled -ErrorAction SilentlyContinue
 }
+
+# Otimizações de registro
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v "UserPreferencesMask" /t REG_BINARY /d 9012038010000000 /f
 
+# Cria o arquivo de emergência (Ativar.bat)
 $bc = @"
 @echo off
 sc config wuauserv start= auto
@@ -19,7 +23,7 @@ pause
 "@
 $bc | Out-File -FilePath $bp -Encoding ascii
 
-# Idioma (Agora no final, não bloqueia o início do sistema)
+# Aplica idioma em segundo plano (não trava o sistema)
 Start-Job -ScriptBlock {
     Install-Language -Language pt-BR
     Set-WinSystemLocale -SystemLocale pt-BR
