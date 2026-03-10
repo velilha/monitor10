b49fc22cf2531f4f176095b0f14c92b6876b0716
velilha <32044809+velilha@users.noreply.github.com>
2026-03-10 04:03:10 -0300

    Update Otimizar.ps1

--- Otimizar.ps1
+++ Otimizar.ps1
@@ -1,16 +1,15 @@
 # Otimizar.ps1
-$log = "C:\Users\runneradmin\Desktop\Otimizacao_Log.txt"
-"Iniciando otimização: $(Get-Date)" | Out-File $log
+$log = "C:\Otimizacao_Log.txt"
+"Iniciando otimização em: $(Get-Date)" | Out-File $log
 
-# 1. Serviços para desativar
+# Lista de serviços
 $servicos = @("wuauserv", "bits", "dosvc", "UsoSvc", "WSearch", "bthserv", "BthAvctpSvc", "Spooler", "DiagTrack", "FeedbackHub", "MapsBroker", "dmwappushservice", "RetailDemo", "lfsvc", "stisvc")
 foreach ($s in $servicos) {
     Stop-Service -Name $s -Force -ErrorAction SilentlyContinue
     Set-Service -Name $s -StartupType Disabled -ErrorAction SilentlyContinue
 }
 
-# 2. Windows Update via Registro
+# Windows Update via Registro
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d 1 /f
 
-"Otimização concluída: $(Get-Date)" | Out-File $log -Append
+"Otimização finalizada com sucesso em: $(Get-Date)" | Out-File $log -Append
