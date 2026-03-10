49373f470163d89eb3ae4997400f845cd2261419
velilha <32044809+velilha@users.noreply.github.com>
2026-03-10 01:13:44 -0300

    Update and rename Otimizar.ps1 to main/Otimizar.ps1

--- main/Otimizar.ps1
+++ main/Otimizar.ps1
@@ -1,12 +1,15 @@
-# Conteúdo do seu arquivo Otimizar.ps1
+# Instala idioma e define PT-BR
 Install-Language pt-BR
 Set-WinSystemLocale -SystemLocale pt-BR
 Set-WinUserLanguageList -LanguageList pt-BR -Force
 Set-WinUILanguageOverride -Language pt-BR
 
+# Desativa serviços pesados
 $servicos = @("wuauserv", "bits", "dosvc", "DiagTrack", "WerSvc", "WbioSrvc", "Spooler", "WSearch", "MapsBroker", "dmwappushservice", "bthserv", "PhoneSvc", "lfsvc")
 foreach ($s in $servicos) {
     Stop-Service -Name $s -Force -ErrorAction SilentlyContinue
     Set-Service -Name $s -StartupType Disabled -ErrorAction SilentlyContinue
 }
+
+# Otimização visual
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Value 2
