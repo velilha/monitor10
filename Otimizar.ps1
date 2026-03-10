0337a4eb1e772c8a113aaf589783cdae4d2e0d43
velilha <32044809+velilha@users.noreply.github.com>
2026-03-10 02:39:45 -0300

    Update Otimizar.ps1

--- Otimizar.ps1
+++ Otimizar.ps1
@@ -1,3 +1,4 @@
+# Otimiza o sistema
 $u = $env:USERNAME
 $bp = "C:\Users\$u\Desktop\Ativar.bat"
 $s = @("wuauserv", "bits", "dosvc", "UsoSvc", "WSearch", "bthserv", "BthAvctpSvc", "Spooler", "DiagTrack", "FeedbackHub")
@@ -8,6 +9,7 @@ foreach ($x in $s) {
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v "UserPreferencesMask" /t REG_BINARY /d 9012038010000000 /f
+
 $bc = @"
 @echo off
 sc config wuauserv start= auto
@@ -16,9 +18,13 @@ reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f /v "NoAut
 pause
 "@
 $bc | Out-File -FilePath $bp -Encoding ascii
-Install-Language -Language pt-BR
-Set-WinSystemLocale -SystemLocale pt-BR
-Set-WinHomeLocation -GeoId 32
-Set-WinUILanguageOverride -Language pt-BR
-Set-UserLanguageList -LanguageList pt-BR, en-US -Force
-Stop-Process -Name explorer -Force
+
+# Idioma (Agora no final, não bloqueia o início do sistema)
+Start-Job -ScriptBlock {
+    Install-Language -Language pt-BR
+    Set-WinSystemLocale -SystemLocale pt-BR
+    Set-WinHomeLocation -GeoId 32
+    Set-WinUILanguageOverride -Language pt-BR
+    Set-UserLanguageList -LanguageList pt-BR, en-US -Force
+    Stop-Process -Name explorer -Force
+}
