f8d91f33e92a3e3dc7848e198cc9023a8052aa60
velilha <32044809+velilha@users.noreply.github.com>
2026-03-10 03:35:30 -0300

    Update Otimizar.ps1

--- Otimizar.ps1
+++ Otimizar.ps1
@@ -1,18 +1,12 @@
 # Otimizar.ps1
-$logPath = "C:\Users\runneradmin\Desktop\Otimizacao_Log.txt"
-"Script Iniciado em: $(Get-Date)" | Out-File $logPath
+$log = "C:\Users\runneradmin\Desktop\Otimizacao_Log.txt"
+"Iniciando em $(Get-Date)" | Out-File $log
 
-$ErrorActionPreference = "SilentlyContinue"
-
-# 1. Desativa serviços
-$servicos = @("wuauserv", "bits", "dosvc", "UsoSvc", "WSearch", "bthserv", "BthAvctpSvc", "Spooler", "DiagTrack", "FeedbackHub")
+# Apenas desativa serviços (sem mexer no explorer.exe)
+$servicos = @("wuauserv", "bits", "dosvc", "UsoSvc", "WSearch")
 foreach ($s in $servicos) {
-    Stop-Service -Name $s -Force
-    Set-Service -Name $s -StartupType Disabled
+    Stop-Service -Name $s -Force -ErrorAction SilentlyContinue
+    Set-Service -Name $s -StartupType Disabled -ErrorAction SilentlyContinue
 }
 
-# 2. Otimizações de registro
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
-
-"Script Finalizado com sucesso em: $(Get-Date)" | Out-File $logPath -Append
+"Finalizado em $(Get-Date)" | Out-File $log -Append
